c2b082ecd00261de90101349585ff145cd4dbaf8
Leonardo Brasil <leo-.-brasil@hotmail.com>
2025-10-23 13:06:52 -0300

    adicionando arquivo local e cloud

--- segunda pagina.py
+++ segunda pagina.py
@@ -4,11 +4,11 @@ import seaborn as sns
 import os
 import streamlit as st
 
-'''def carregar_dados(csv_name):
+def carregar_dados(csv_name):
     df = pd.read_csv(csv_name, encoding="latin1", sep=";")
     return df
 
 # Carrega e exibe os dados
-df = carregar_dados('dados_reduzidos.csv')'''
+df = carregar_dados('dados_reduzidos.csv')
 st.title("Dados Reduzidos do ENEM 2023")
-#st.dataframe(df.head(50))
+st.dataframe(df.head(50))
